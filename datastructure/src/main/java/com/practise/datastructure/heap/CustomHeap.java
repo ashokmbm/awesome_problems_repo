a3483c30f4ea308220997f56135864b54a40d2f0
ashokmbm <ashoksinghmbm@gmail.com>
2021-01-17 21:54:13 +0530

    custom heap class added

---
+++ datastructure/src/main/java/com/practise/datastructure/heap/CustomHeap.java
@@ -0,0 +1,13 @@
+package com.practise.datastructure.heap;
+/*
+     Heap is priority queue where most priority element will be remove first
+     We can represent heap as complete binary tree (except leaf nodes) where parent node always have higher priority than it children.
+     Leaf nodes will be added from left to right direction.
+     Two type of heaps Min Heap / Max Heap
+     We can implement heap by using array list to ful fill property of left to right filling of leaf nodes.
+
+ */
+
+public class CustomHeap {
+
+}
